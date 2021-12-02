615b71f86f33f3d49426a1dbcf34bb1e2d8cb562
xavireina <xavierreina93@gmail.com>
2021-12-02 10:49:00 +0100

    done

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+
+
+
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -14,14 +18,88 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
+
+  //Connexió amb la base de dades...
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
+
+  // Afegim una recepta...
+  .then(() => { 
+    const my_recipe = {
+      title: "Salmorejo",
+      level: "Easy Peasy",
+      ingredients:["tomato", "oil", "bread"],
+      cuisine: "Andaluza",
+      dishType: "soup",
+      image: "https://images.media-allrecipes.com/images/75131.jpg",
+      duration: 50, 
+      creator: "Xavi",
+      // created: Date.now
+    }
+      return Recipe.create(my_recipe, (error, receta) => {
+        if (error) {
+          console.log('An error happened:', error);
+          return;
+        }
+        console.log('The recipe is saved and its value is: ', receta.title);
+      });
+
+  }) 
+  //Insertem totes les receptes del fitxer data.json
+  .then(() => { 
+      return Recipe.insertMany(data, (error, recetas) => {
+        if (error) {
+          console.log('An error happened:', error);
+          return;
+        }
+        console.log('The amount of recetas are: ', recetas.length);
+      });
+
   })
+
+  //Modifiquem atribut d'un element 
+  .then(() => { 
+    return Recipe.findOneAndUpdate(
+      {
+        title: "Rigatoni alla Genovese"
+      }, 
+      {
+        duration: 100
+      },
+    );
+
+  })
+
+  //Eliminem un element
+  .then(() => { 
+    return Recipe.deleteOne({title: "Carrot Cake"})
+
+  })
+
+
+// Busquem tots els elements 
+  .then(() => { 
+    return Recipe.find({})
+    .then(recetas => console.log(recetas))
+  })
+
+// Ens desconectem
+  .then(() => { 
+    mongoose.connection.close();
+    console.log("connection closed")
+  })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  
+
+
+
+
+  
+  
